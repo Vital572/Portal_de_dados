a4d45a947767564d63863e5723016e9ea762e2df
Vital572 <vitalsalaquiacomanuel@gmail.com>
2025-05-01 19:44:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,15 @@
 self.addEventListener('install', function (event) {
     console.log('Service Worker instalado');
-    self.skipWaiting(); // ativa imediatamente
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', function (event) {
     console.log('Service Worker ativado');
 });
 
-// Para mostrar notificação se o navegador quiser
 self.addEventListener('notificationclick', function (event) {
     event.notification.close();
     event.waitUntil(
-        clients.openWindow("https://seudominio.com/novidade") // ou a página de novidades
+        clients.openWindow("https://seudominio.com/novidade")
     );
 });
