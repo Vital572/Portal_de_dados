523e22e6d5db91259e0164f17a15d1ebad96d688
Vital572 <vitalsalaquiacomanuel@gmail.com>
2025-05-01 22:00:14 +0100

    firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,20 +1,20 @@
-// firebase-messaging-sw.js
-importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging.js');
 
 firebase.initializeApp({
-  apiKey: "102524887029",
-  authDomain: "https://portal-de-dados.vercel.app.firebaseapp.com",
-  projectId: "Portal de Dados de Angola",
-  messagingSenderId: "BMYKIODIlW81cUFGC0YFMjyngdLEzqU3-73SYBKbqP7ymtdJVa2JPHkJVnBUFF98KRtlUgRluFV8TX0kEgcAuEs",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyAfexQ--FwF8NsLc_lSI4m1kDGjWbd6rfc",
+  authDomain: "portal-de-dados-de-angola.firebaseapp.com",
+  projectId: "portal-de-dados-de-angola",
+  messagingSenderId: "102524887029",
+  appId: "1:102524887029:web:a4e2597f437ba01c700abc"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Mensagem recebida:', payload);
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: "/icon.png"
+    icon: '/icon.png' // Você pode substituir por seu ícone
   });
 });
